928811fe52834aba54d9feee9a1fb1d7ec15df40
Maria <marylevchenko@gmail.com>
2015-03-19 16:50:05 +0000

    Morpho search final version

--- controllers/morpho.py
+++ controllers/morpho.py
@@ -2,20 +2,21 @@
 import pymorphy2
 morph = pymorphy2.MorphAnalyzer()
 
-names = ['Abbr', 'Name', 'Surn', 'Patr', 'Geox', 'Init']
+names = ['None', 'Abbr', 'Name', 'Surn', 'Patr', 'Geox', 'Init']
 style = ['Infr', 'Slng', 'Arch', 'Litr', 'Erro', 'Dist']
 other = ['Ques', 'Dmns', 'Prnt', 'Prdx', 'Af-p', 'Anph', 'Inmx', 'V-be', 'V-en', 'V-ie', 'V-bi', 'V-ey', 'V-oy', 'Cmp2', 'V-ej', 'Fimp', 'Coun', 'Coll', 'V-sh', 'Vpre', 'Supr', 'Qual', 'Apro', 'Anum', 'Poss']
-partos = ['NOUN', 'ADJF', 'ADJS', 'VERB', 'INFN', 'GRND', 'PRTF', 'PRTS', 'ADVB', 'PREP', 'CONJ', 'NPRO', 'PRCL' ]
+
+partos = ['None', 'NOUN', 'ADJF', 'ADJS', 'VERB', 'INFN', 'GRND', 'PRTF', 'PRTS', 'ADVB', 'PREP', 'CONJ', 'NPRO', 'PRCL' ]
 animacy = ['None','anim', 'inan']
 gender = ['None','masc', 'femn', 'neut', 'Ms-f']
-number = ['sing', 'plur', 'Sgtm', 'Pltm']
+number = ['None', 'sing', 'plur', 'Sgtm', 'Pltm']
 case = ['None','nomn', 'gent', 'datv', 'accs', 'ablt', 'loct', 'voct', 'gen1', 'gen2', 'acc2', 'loc1', 'loc2']
-aspect = ['perf', 'impf']
-trans = ['tran', 'intr']
-person = ['1per', '2per', '3per']
-tenses = ['past', 'pres', 'futr']
-mood = ['indc', 'impr']
-voice = ['actv', 'pssv']
+aspect = ['None', 'perf', 'impf']
+trans = ['None', 'tran', 'intr']
+person = ['None', '1per', '2per', '3per']
+tenses = ['None', 'past', 'pres', 'futr']
+mood = ['None', 'indc', 'impr']
+voice = ['None', 'actv', 'pssv']
 
 def index():
     authors = trymysql().select(trymysql.author.ALL, orderby=trymysql.author.id)
@@ -48,17 +49,17 @@ def count_partos(): # count categories by author
     for each in authors:
         family = trymysql(trymysql.author.id==each).select()[0]
         result = [family['family']]
-        for all in case:
+        for all in case[1:]:
             count = trymysql((trymysql.allword.author==each)&(trymysql.allword.cas==all)).count()
             total = trymysql((trymysql.allword.author==each)&(trymysql.allword.cas!='None')).count()
             proc = round((float((count)*1.0)/int(total))*100, 3)
             result.append(proc)
-        for all in partos:
+        for all in partos[1:]:
             count = trymysql((trymysql.allword.author==each)&(trymysql.allword.partos==all)).count()
             total = trymysql((trymysql.allword.author==each)&(trymysql.allword.partos.belongs(partos))).count()
             proc = round((float((count)*1.0)/int(total))*100, 2)
             result.append(proc)
-        for all in person:
+        for all in person[1:]:
             count = trymysql((trymysql.allword.author==each)&(trymysql.allword.person==all)).count()
             total = trymysql((trymysql.allword.author==each)&(trymysql.allword.person!='None')).count()
             proc = round((float((count)*1.0)/int(total))*100, 2)
@@ -90,11 +91,19 @@ def count_tense_group():
 
 def search():
     options2 = [OPTION(texts.name, " ", texts.family, _value=texts.id) for texts in trymysql().select(trymysql.author.ALL)]
-    form=FORM(TABLE(TR("Часть речи",SELECT(*partos, _name="first")),
+    form=FORM(TABLE(TR("Автор", SELECT(*options2, _name="author")),
+                    TR("Часть речи",SELECT(*partos, _name="first")),
                     TR("Падеж",SELECT(*case, _name="second")),
                     TR("Одушевленность",SELECT(*animacy, _name="animacy")),
                     TR("Род", SELECT(*gender, _name="gender")),
-                    TR("Автор", SELECT(*options2, _name="author")),
+                    TR("Время", SELECT(*tenses, _name="tenses")),
+                    TR("Вид", SELECT(*aspect, _name = "aspect")),
+                    TR("Залог", SELECT(*voice, _name = "voice")),
+                    TR("Лицо", SELECT(*person, _name = "person")),
+                    TR("Переходность", SELECT(*trans, _name = "trans")),
+                    TR("Число", SELECT(*number, _name = "number")),
+                    TR("Наклонение", SELECT(*mood, _name="mood")),
+                    TR("Имена собственные", SELECT(*names, _name="names")),
                     TR("",INPUT(_type="submit",_value="SUBMIT"))))
     if form.process(formname='form_one').accepted:
         redirect(URL('search_result', args =
@@ -102,7 +111,15 @@ def search():
                       form.vars['second'],
                       form.vars['animacy'],
                       form.vars['gender'],
-                      form.vars['author']]))
+                      form.vars['author'],
+                      form.vars['tenses'],
+                      form.vars['aspect'],
+                      form.vars['voice'],
+                      form.vars['person'],
+                      form.vars['trans'],
+                      form.vars['number'],
+                      form.vars['mood'],
+                      form.vars['names']]))
         response.flash="form accepted"
     return dict(form=form)
 
@@ -112,24 +129,52 @@ def search_result():
     anim = request.args(2)
     gender = request.args(3)
     author = request.args(4)
-    words = trymysql((trymysql.allword.partos==request.args(0))&
-                     (trymysql.allword.anim==request.args(2))&
-                     (trymysql.allword.gendr==request.args(3))&
-                     (trymysql.allword.author==request.args(4))&
-                     (trymysql.allword.cas==request.args(1))).select()
-    strings =[]
-    for all in words[:30]:
-        location = all.text_location
-        title1 = trymysql(trymysql.text1.id==int(all.title)).select()[0]
-        title=title1.title
-        author= (title1.author.name, title1.author.family)
-        string="..."
-        for x in range(all.id-6, all.id+6):
-            for_string = trymysql(trymysql.allword.id==x).select()[0]
-            string= string + " " + str(for_string.lemma)
-        strings.append((string +"...", title, author, int(title1.id)))
-    return dict(p = selected_partos, f = selected_form, strings=strings)
-    
+    tense = request.args(5)
+    words = trymysql(trymysql.allword.author==request.args(4)).select(trymysql.allword.id)
+    if request.args(0) != 'None':
+        words = trymysql((trymysql.allword.id.belongs(words))&(trymysql.allword.partos==request.args(0))).select()
+    if request.args(1) != 'None':
+        words = trymysql((trymysql.allword.id.belongs(words))&(trymysql.allword.cas==request.args(1))).select()
+    if request.args(2) != 'None':
+        words = trymysql((trymysql.allword.id.belongs(words))&(trymysql.allword.anim==request.args(2))).select()
+    if request.args(3) != 'None':
+        words = trymysql((trymysql.allword.id.belongs(words))&(trymysql.allword.gendr==request.args(3))).select()
+    if request.args(5) != 'None':
+        words = trymysql((trymysql.allword.id.belongs(words))&(trymysql.allword.tense==request.args(5))).select()
+    if request.args(6) != 'None':
+        words = trymysql((trymysql.allword.id.belongs(words))&(trymysql.allword.aspect==request.args(6))).select()
+    if request.args(7) != 'None':
+        words = trymysql((trymysql.allword.id.belongs(words))&(trymysql.allword.voice==request.args(7))).select()
+    if request.args(8) != 'None':
+        words = trymysql((trymysql.allword.id.belongs(words))&(trymysql.allword.person==request.args(8))).select()
+    if request.args(9) != 'None':
+        words = trymysql((trymysql.allword.id.belongs(words))&(trymysql.allword.transitivity==request.args(9))).select()
+    if request.args(10) != 'None':
+        words = trymysql((trymysql.allword.id.belongs(words))&(trymysql.allword.number==request.args(10))).select()
+    if request.args(11) != 'None':
+        words = trymysql((trymysql.allword.id.belongs(words))&(trymysql.allword.mood==request.args(11))).select()
+    if request.args(12) != 'None':
+        words = trymysql((trymysql.allword.id.belongs(words))&(trymysql.allword.sobstv==request.args(12))).select()
+    message=""
+    strings=[]
+    if len(words) == 0:
+        message = "Sorry, nothing found"
+    else:
+        for all in words:
+            location = all.text_location
+            title1 = trymysql(trymysql.text1.id==int(all.title)).select()[0]
+            title=title1.title
+            author= (title1.author.name, title1.author.family)
+            string="..."
+            for x in range(all.id-6, all.id+6):
+                try:
+                    for_string = trymysql(trymysql.allword.id==x).select()[0]
+                    string= string + " " + str(for_string.lemma)
+                except:
+                    pass
+            strings.append((string +"...", title, author, int(title1.id)))
+    return dict(strings=strings, message=message)
+
 def draft():
     base_id=[int(all.id) for all in trymysql(trymysql.text1.group_text==request.args(0)).select()]
     base_id0=[int(all.id) for all in trymysql(trymysql.text1.group_text==request.args(1)).select()]
