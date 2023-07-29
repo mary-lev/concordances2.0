a64c4a1ec54a881913072c709856f9f41e372078
mary-lev <marylevchenko@gmail.com>
2023-07-29 21:11:26 +0200

    Docker compose

---
+++ backend/app/schemas.py
@@ -0,0 +1,252 @@
+from typing import Optional
+from pydantic import BaseModel, ConfigDict
+from typing import List
+
+class AuthorBase(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+    name: Optional[str] = None
+    surname: Optional[str] = None
+    family: Optional[str] = None
+    real_name: Optional[str] = None
+    year_birth: Optional[str] = None
+    year_death: Optional[str] = None
+    gender: Optional[str] = None
+    birth_location: Optional[str] = None
+    death_location: Optional[str] = None
+
+class AuthorCreate(AuthorBase):
+    name: str
+
+class AuthorUpdate(AuthorBase):
+    pass
+
+class AuthorInDBBase(AuthorBase):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+class PublicationBase(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+    title: Optional[str]
+    city: Optional[str] = None
+    editor: Optional[str] = None
+    publisher: Optional[str] = None
+    year: Optional[str] = None
+    part: Optional[str] = None
+    short: Optional[str] = None
+    author_id: Optional[int] = None
+
+
+class PublicationCreate(PublicationBase):
+    title: str
+
+class PublicationUpdate(PublicationBase):
+    pass
+
+class PublicationInDBBase(PublicationBase):
+    id: int
+    title: str
+
+    class Config:
+        orm_mode = True
+
+class LocationBase(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+    name: Optional[str] = None
+    country: Optional[str] = None
+    city: Optional[str] = None
+    address: Optional[str] = None
+    latitude: Optional[float] = None
+    longitude: Optional[float] = None
+
+
+class LocationCreate(LocationBase):
+    name: str
+
+class LocationUpdate(LocationBase):
+    pass
+
+class LocationInDBBase(LocationBase):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+class TextDateBase(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+    year: Optional[int] = None
+    month: Optional[int] = None
+    day: Optional[int] = None
+    dubious_year: Optional[str] = None
+    dubious_month: Optional[str] = None
+    dubious_day: Optional[str] = None
+    start_year: Optional[int] = None
+    end_year: Optional[int] = None
+    season: Optional[str] = None
+    comment: Optional[str] = None
+
+
+class TextDateCreate(TextDateBase):
+    pass
+
+class TextDateUpdate(TextDateBase):
+    pass
+
+class TextDateInDBBase(TextDateBase):
+    id: int
+    year: int
+    month: int
+    day: int
+    dubious_day: str
+    dubious_month: str
+    dubious_year: str
+    start_year: int
+    end_year: int
+    season: str
+
+    class Config:
+        orm_mode = True
+
+
+class GroupTextBase(BaseModel):
+    location_id: Optional[int] = None
+    location: Optional[LocationBase] = None
+    author_id: int
+    title: Optional[str] = None
+    dedication: Optional[str] = None
+    subtitle: Optional[str] = None
+    author_comment: Optional[str] = None
+    text_date_id: Optional[int] = None
+    location_id: Optional[int] = None
+
+
+class GroupTextCreate(GroupTextBase):
+    title: str
+
+class GroupTextUpdate(GroupTextBase):
+    pass
+
+class GroupTextInDBBase(GroupTextBase):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+
+class TextBase(BaseModel):
+    text_id: int
+    title: Optional[str] = None
+    subtitle: Optional[str] = None
+    first_string: Optional[str] = None
+    body: Optional[str] = None
+    filename: Optional[str]
+    source: Optional[str] = None
+    n_in_group: int = None
+    author: Optional[AuthorBase] = None
+    author_id: int
+    author_comment: Optional[str] = None
+    dedication: Optional[str] = None
+    group_text_id: Optional[int] = None
+    group_text: Optional[GroupTextBase] = None
+    publication_id: Optional[int] = None
+    publication: Optional[PublicationBase] = None
+    location_id: Optional[int] = None
+    location: Optional[LocationBase] = None
+    text_date_id: Optional[int] = None
+    text_date: Optional[TextDateBase] = None
+
+
+class TextCreate(TextBase):
+    body: str
+
+class TextUpdate(TextBase):
+    pass
+
+class TextInDBBase(TextBase):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+
+class EpigraphBase(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+    epi_text: Optional[str] = None
+    epi_author: Optional[str] = None
+    epi_book: Optional[str] = None
+    epi_filename: Optional[str] = None
+    text_id: Optional[int] = None
+
+class EpigraphCreate(EpigraphBase):
+    epi_text: str
+
+class EpigraphSchema(EpigraphBase):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+class VariantBase(BaseModel):
+    first_string: Optional[str] = None
+    filename: Optional[str] = None
+    body: Optional[str] = None
+    title: Optional[str] = None
+    subtitle: Optional[str] = None
+    author_comment: Optional[str] = None
+    dedication: Optional[str] = None
+    year: Optional[str] = None
+    date: Optional[str] = None
+    publication: Optional[PublicationBase] = None
+    publication_id: Optional[int] = None
+    location_id: Optional[int] = None
+    location: Optional[LocationBase] = None
+    book_page_start: Optional[int] = None
+    book_page_end: Optional[int] = None
+    variant_of_text: Optional[TextBase] = None
+    variant_of_text_id: int
+    epi_text: Optional[str] = None
+    epigraph: Optional[EpigraphBase] = None
+    epi_author: Optional[str] = None
+    author: Optional[AuthorBase] = None
+    author_id: int
+    text_date_id: Optional[int] = None
+    text_date: Optional[TextDateBase] = None
+
+class VariantCreate(VariantBase):
+    filename: str
+    variant_of_text_id: int
+
+class VariantUpdate(VariantBase):
+    pass
+
+class VariantInDBBase(VariantBase):
+    id: int
+    publication: PublicationBase
+
+    class Config:
+        orm_mode = True
+
+
+class OldBase(BaseModel):
+    first_string: Optional[str] = None
+    filename: Optional[str]
+    date: Optional[str] = None
+    body: Optional[str] = None
+    old_variant_of_text: Optional[TextBase] = None
+    old_variant_of_text_id: int
+
+
+class OldCreate(OldBase):
+    filename: str
+
+class OldUpdate(OldBase):
+    pass
+
+class OldInDBBase(OldBase):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+
---
+++ backend/app/utils.py
@@ -0,0 +1,72 @@
+import pandas as pd
+import app.crud as crud
+import app.schemas as schemas
+
+
+def parse_year(year):
+    if isinstance(year, float):
+        return year, None, None
+    start_year = None
+    end_year = None
+    if "—" in year:
+        start_year = int(year.split("—")[0])
+        end_year = int(year.split("—")[1])
+    elif "–" in year:
+        start_year = int(year.split("–")[0])
+        end_year = int(year.split("–")[1])
+    elif "-" in year:
+        start_year, end_year = year.split("-")
+    if start_year:
+        year = start_year
+    return int(year), start_year, end_year
+
+
+def parse_to_int(value):
+    if not value:
+        return None
+    if isinstance(value, float):
+        return int(value)
+    if isinstance(value, str):
+        return int(value.strip())
+    return value
+
+
+def get_date(row, db, table_name):
+    year = row.get(f'{table_name}.year_writing')
+    month = row.get(f'{table_name}.month_writing')
+    day = row.get(f'{table_name}.day_writing')
+    dyear = row.get(f'{table_name}.dyear_writing')
+    dmonth = row.get(f'{table_name}.dmonth_writing')
+    dday = row.get(f'{table_name}.dday_writing')
+    season = row.get(f'{table_name}.season_writing')
+    if table_name == 'group_text':
+        year = row["group_text.year_writing"]
+    if pd.notna(year) and year != '':
+        year, start_year, end_year = parse_year(year)
+        date = crud.get_exact_date(
+            db=db,
+            year=parse_to_int(year),
+            month=parse_to_int(month),
+            day=parse_to_int(day),
+            dubious_year=dyear,
+            dubious_month=dmonth,
+            dubious_day=dday,
+            season=season,
+            start_year=start_year,
+            end_year=end_year,
+        )
+        if not date:
+            date_data = schemas.TextDateCreate(**{
+                "year": parse_to_int(year),
+                "month": parse_to_int(month),
+                "day": parse_to_int(day),
+                "dubious_year": dyear,
+                "dubious_month": dmonth,
+                "dubious_day": dday,
+                "season": season,
+                "start_year": start_year,
+                "end_year": end_year,
+            })
+            date = crud.create_date(db=db, date=date_data)
+        return date.id
+    return None
